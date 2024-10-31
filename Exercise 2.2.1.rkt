1731d947876ecbc54b05acae330168895a111aaa
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-10-31 15:22:48 +0500

    fix: Update Exercise 2.2.1.rkt

--- Exercise 2.2.1.rkt
+++ Exercise 2.2.1.rkt
@@ -1,6 +1,3 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-advanced-reader.ss" "lang")((modname |Exercise 2.2.1|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; The Scheme Programming Language Fourth Edition
 
 ;; Solutions By : Alexander Atamanyuk
@@ -12,6 +9,8 @@
 ;; b. (2/3 + 4/9) ÷ (5/11 - 4/3)
 ;; c. 1 + 1 ÷ (2 + 1 ÷ (1 + 1/2))
 ;; d. 1 × -2 × 3 × -4 × 5 × -6 × 7
+
+#lang scheme
 
 (+ (* 1.2 (- 2 1/3)) (- 8.7))
 
