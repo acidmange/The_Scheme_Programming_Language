d092ebe28f4bdbde73c9259818559857cc08ebfe
Alexander Atamanyuk <psotiframe13@gmail.com>
2024-11-01 13:42:51 +0500

    feat: add exercise 2.2.8

---
+++ Exercise 2.2.8.rkt
@@ -0,0 +1,16 @@
+;; The Scheme Programming Language Fourth Edition
+
+;; Solutions By : Alexander Atamanyuk
+
+;; Exercise 2.2.8
+
+;; Try to explain how Scheme expressions are evaluated. Does your explanation cover the last example in
+;; Exercise 2.2.3?
+
+#lang scheme
+
+;; Lisp starts calculating expressions starting from the deepest operation.
+;; He pays attention to the very first (left) character in the expression to
+;; find out if the character is part of the list or if it is an operator/procedure.
+;; Next, he looks at the rest of the expression. If there is a quote character in front of the symbol,
+;; then it treats operands or arguments as part of the list, otherwise it accesses them directly
